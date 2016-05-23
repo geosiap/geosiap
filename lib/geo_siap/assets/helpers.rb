13963e42d4fa60d37c6332de61f04665c6a7eb9b
Fábio Rodrigues <fabio.info@gmail.com>
2016-05-23 17:31:50 -0300

    v1.0.7

--- lib/geo_siap/assets/helpers.rb
+++ lib/geo_siap/assets/helpers.rb
@@ -2,14 +2,14 @@ module GeoSiap::Assets::Helpers
 
   include ActionView::Helpers::AssetTagHelper
 
-  GEO_ASSETS_HOST = '//designa.github.io/geo-framework'
+  GEO_ASSETS_HOST = '//github.com/embras/geosiap-assets'
 
   def geo_stylesheet_link_tag
-    stylesheet_link_tag "#{GEO_ASSETS_HOST}/dist/css/geo-framework.min.css", media: 'all'
+    stylesheet_link_tag "#{GEO_ASSETS_HOST}/dist/1.2/css/geo-framework.min.css", media: 'all'
   end
 
   def geo_javascript_include_tag
-    javascript_include_tag("#{GEO_ASSETS_HOST}/dist/js/geo-framework.min.js")
+    javascript_include_tag("#{GEO_ASSETS_HOST}/dist/1.2/js/geo-framework.min.js")
   end
 
   def geo_image_tag(name, options={})
